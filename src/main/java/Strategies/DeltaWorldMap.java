37647a991beb0daf4860cde9251282dbb5686a2a
col825 <97612688+col825@users.noreply.github.com>
2022-09-30 13:56:01 -0400

    Included rest of world layout, tests not passed for checking tiles

--- src/main/java/Strategies/DeltaWorldMap.java
+++ src/main/java/Strategies/DeltaWorldMap.java
@@ -1,5 +1,9 @@
 package Strategies;
 
+import hotciv.framework.*;
+
+import java.util.*;
+
 import hotciv.framework.Player;
 import hotciv.framework.Position;
 import hotciv.standard.CityImpl;
@@ -27,5 +31,47 @@ public class DeltaWorldMap implements WorldMap{
         tileMap.put(oceanPos, new TileImpl(oceanPos, OCEANS));
         tileMap.put(hillPos, new TileImpl(oceanPos, HILLS));
         tileMap.put(mountainPos, new TileImpl(oceanPos, MOUNTAINS));
+    }
+    private Map<Position,Tile> defineWorld() {
+        // Basically we use a 'data driven' approach - code the
+        // layout in a simple semi-visual representation, and
+        // convert it to the actual Game representation.
+        String[] layout =
+                new String[] {
+                        "...ooMooooo.....",
+                        "..ohhoooofffoo..",
+                        ".oooooMooo...oo.",
+                        ".ooMMMoooo..oooo",
+                        "...ofooohhoooo..",
+                        ".ofoofooooohhoo.",
+                        "...ooo..........",
+                        ".ooooo.ooohooM..",
+                        ".ooooo.oohooof..",
+                        "offfoooo.offoooo",
+                        "oooooooo...ooooo",
+                        ".ooMMMoooo......",
+                        "..ooooooffoooo..",
+                        "....ooooooooo...",
+                        "..ooohhoo.......",
+                        ".....ooooooooo..",
+                };
+        // Conversion...
+        Map<Position,Tile> theWorld = new HashMap<Position,Tile>();
+        String line;
+        for ( int r = 0; r < GameConstants.WORLDSIZE; r++ ) {
+            line = layout[r];
+            for ( int c = 0; c < GameConstants.WORLDSIZE; c++ ) {
+                char tileChar = line.charAt(c);
+                String type = "error";
+                if ( tileChar == '.' ) { type = GameConstants.OCEANS; }
+                if ( tileChar == 'o' ) { type = GameConstants.PLAINS; }
+                if ( tileChar == 'M' ) { type = GameConstants.MOUNTAINS; }
+                if ( tileChar == 'f' ) { type = GameConstants.FOREST; }
+                if ( tileChar == 'h' ) { type = GameConstants.HILLS; }
+                Position p = new Position(r,c);
+                //theWorld.put( p, new StubTile(type));
+            }
+        }
+        return theWorld;
     }
 }
--- src/test/java/hotciv/standard/TestDeltaCiv.java
+++ src/test/java/hotciv/standard/TestDeltaCiv.java
@@ -27,4 +27,25 @@ public class TestDeltaCiv {
     public void ReturnBlueCityForPositionFourFive() {
         assertThat(game.getCityAt(new Position(4,5)).getOwner(), is(Player.BLUE));
     }
+    @Test
+    public void ReturnOceansAtPositionFiveTwo() {
+        assertThat(game.getTileAt(new Position(5,2)).getTypeString(), is(OCEANS));
+    }
+    @Test
+    public void ReturnPlainsAtPositionThreeThree() {
+        assertThat(game.getTileAt(new Position(3,3)).getTypeString(), is(PLAINS));
+    }
+    @Test
+    public void ReturnMountainsAtPositionOneSix() {
+        assertThat(game.getTileAt(new Position(1,6)).getTypeString(), is(MOUNTAINS));
+    }
+    @Test
+    public void ReturnForestAtPositionNineFourteen() {
+        assertThat(game.getTileAt(new Position(9,14)).getTypeString(), is(FOREST));
+    }
+    @Test
+    public void ReturnHillsAtPositionFifteenFive() {
+        assertThat(game.getTileAt(new Position(15,5)).getTypeString(), is(HILLS));
+    }
+
 }
